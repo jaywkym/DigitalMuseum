7b34064e7c5117964e4e78e7d3442253e5990ebd
jaywkym <j.w.kim@wustl.edu>
2023-02-11 15:49:23 -0600

    homefeedAppbarBottomNav

--- daily-getty/pages/profile.tsx
+++ daily-getty/pages/profile.tsx
@@ -72,36 +72,6 @@ export default function Home() {
             </Head>
             <main>
                 <Box sx={{ flexGrow: 1 }}>
-                    <AppBar position="static">
-                        <Toolbar>
-                            <IconButton
-                                size="large"
-                                edge="start"
-                                color="inherit"
-                                aria-label="open drawer"
-                                sx={{ mr: 2 }}
-                            >
-                                <MenuIcon />
-                            </IconButton>
-                            <Typography
-                                variant="h6"
-                                noWrap
-                                component="div"
-                                sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-                            >
-                                DailyMuse
-                            </Typography>
-                            <Search>
-                                <SearchIconWrapper>
-                                    <SearchIcon />
-                                </SearchIconWrapper>
-                                <StyledInputBase
-                                    placeholder="Search…"
-                                    inputProps={{ 'aria-label': 'search' }}
-                                />
-                            </Search>
-                        </Toolbar>
-                    </AppBar>
                     <CssBaseline />
                     <List>
                     </List>
