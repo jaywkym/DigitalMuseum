5f67fc172676c7a7c70b83cebd11ed3bc3045b49
jaywkym <j.w.kim@wustl.edu>
2023-02-13 17:55:08 -0600

    test

--- daily-getty/pages/profile.tsx
+++ daily-getty/pages/profile.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import Head from 'next/head'
 import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
+import { Avatar } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import HomeIcon from '@mui/icons-material/Home';
@@ -18,48 +18,7 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import Paper from '@mui/material/Paper';
 import List from '@mui/material/List';
 import Link from 'next/link';
-
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(1),
-        width: 'auto',
-    },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            width: '12ch',
-            '&:focus': {
-                width: '20ch',
-            },
-        },
-    },
-}));
+import { Container } from '@mui/system';
 
 export default function Home() {
 
@@ -68,13 +27,48 @@ export default function Home() {
     return (
         <>
             <Head>
-                <title>Home Feed</title>
+                <title>Profile Page</title>
             </Head>
             <main>
                 <Box sx={{ flexGrow: 1 }}>
-                    <CssBaseline />
-                    <List>
-                    </List>
+                    <Container fixed>
+                        <div>
+                            <h1>
+                                Hey, @USERNAME!
+                            </h1>
+                        </div>
+                        <div>
+                            <Avatar
+                                alt="Remy Sharp"
+                                src="/public/vercel.svg"
+                                sx={{ width: 56, height: 56 }}
+                            />
+                        </div>
+                        <CssBaseline />
+                        <List>
+                            <Card sx={{ maxWidth: 345 }}>
+                                <CardMedia
+                                    component="img"
+                                    alt="green iguana"
+                                    height="140"
+                                    image="/static/images/cards/contemplative-reptile.jpg"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        Lizard
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                                        species, ranging across all continents except Antarctica
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small">Share</Button>
+                                    <Button size="small">Learn More</Button>
+                                </CardActions>
+                            </Card>
+                        </List>
+                    </Container>
                     <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
                         <BottomNavigation
                             showLabels
@@ -83,7 +77,7 @@ export default function Home() {
                                 setValue(newValue);
                             }}
                         >
-                            <Link href="/homefeed">
+                            <Link href="/homefeed" >
                                 <BottomNavigationAction label="Feed" icon={<HomeIcon />} />
                             </Link>
                             <Link href="/newmuse">
