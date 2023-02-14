e69511c00abae56f74b12fcf8599b76e6db6c469
wsilber <silberstein75@gmail.com>
2023-02-14 12:35:21 -0600

    comments for user authentication

--- daily-getty/pages/api/auth/[...nextauth].tsx
+++ daily-getty/pages/api/auth/[...nextauth].tsx
@@ -6,8 +6,13 @@ type LoginCredentials = {
     password: string
 }
 
+/**
+ * authOptions: Configuration for authentication through next. 
+ */
 export const authOptions = {  
-    secret: process.env.SECRET,
+    secret: process.env.SECRET, // TODO - Get Jay to add a secret key to sign session/ JWT tokens
+
+    /* Defines the types of ways that a user can login to the platform */
     providers: [    
         CredentialsProvider({
             name: 'Credentials',
@@ -15,7 +20,7 @@ export const authOptions = {
                 username: { label: "Username or Phone Number", type: "text", placeholder: "username or phone" },
                 password: { label: "Password", type: "password", placeholder: "**********" }
             },
-            authorize(credentials, req) {
+            async authorize(credentials, req) {
                 const { username, password } = credentials as LoginCredentials
 
                 /* TODO - pull user from database. Reject if not known or
@@ -27,6 +32,8 @@ export const authOptions = {
             }
         }) 
     ],
+
+    /* Custom pages that will direct the user to the provider's login page */
     pages: {
         /* TODO - Add custom sign in pages for multiple providers. Add error
                   and sign out pages if needed */
