7bac27672b376b4840f7bc704cf47087c6461ca5
William Silberstein <silberstein75@gmail.com>
2023-02-15 12:13:59 -0600

    Added custom hook for generating dalle images

--- daily-getty/pages/auth/signin.tsx
+++ daily-getty/pages/auth/signin.tsx
@@ -1,35 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { signIn, signOut } from 'next-auth/react'
+import useImage from '../dalle/images'
 import Image from 'next/image';
-
-type ImageResponse = {
-    success: boolean,
-    amount : number,
-    image  : DalleResponse,
-    error  : DalleError,
-}
-
-type DalleResponse = {
-    created: number,
-    data   : {
-        b64_json: string
-    }[],
-    success: boolean
-}
-
-type DalleError = {
-    code: number,
-    message: string,
-    param: string,
-    type: string 
-}
 
 function SignInPage() {
 
-    const [b64_image, setImage ] = useState('')
     const [prompt, setPrompt] = useState('')
     const [amount, setAmount] = useState('')
-
+    const [ b64_image, error, generateImage ] = useImage(prompt, amount);
 
     return (
         <>
@@ -46,37 +24,12 @@ function SignInPage() {
 
             <input type={'text'} value={prompt} onChange={(e) => setPrompt(e.target.value)}/>
             <input type={'number'} step={'1'} value={amount} onChange={(e) => setAmount(e.target.value)}/>
-            <input type={'button'} onClick={() => {
-               const request = {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        'prompt': prompt,
-                        'amount': amount
-                    })
-                }
-        
-                console.log("Fetching image")
-                console.log(request)
-            
-                fetch('/api/dalle/generate_image', request)
-                .then(res => res.json())
-                .then(resj => {
-                    const res = resj as ImageResponse
-                    console.log(res)
-                    if(res.success) {
-                        setImage(`data:image/png;base64, ${res.image.data[0].b64_json}`)
-                    }
-                })
-        
-            }} value={'Generate Image'} />
+            <input type={'button'} onClick={generateImage} value={'Generate Image'} />
 
             <br></br>
             <br></br>
 
-            <Image src={b64_image} alt={"Base 64 Image"} width={500} height={500}></Image>
+            <Image src={b64_image !== null? b64_image : "/"} alt={"Base 64 Image"} width={500} height={500}></Image>
         </>
     )
 }
