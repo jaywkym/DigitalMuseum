ce4f951f5a27147ae75e1598ed1890963c6b285d
William Silberstein <silberstein75@gmail.com>
2023-02-22 11:46:51 -0600

    cleanup

--- daily-getty/pages/api/auth/[...nextauth].tsx
+++ daily-getty/pages/api/auth/[...nextauth].tsx
@@ -12,35 +12,6 @@ import type {
     DatabaseUserResponse
 }  from "../../../types/FirebaseResponseTypes";
 
-type LoginCredentials = {
-    username: string
-    password: string
-}
-
-type SignInResponse = {
-    user: string,
-    account: string,
-    profile: string,
-    email: string,
-    credentials: string
-}
-
-type User = {
-    uuid: string,
-    email: string,
-    google_id: string,
-    username: string,
-    profile_picture: string,
-    friends: User[],
-    create_time: number
-}
-
-type CustomUserToken = {
-    user_id: string,
-    email  : string,
-    username : string
-}
-
 type CustomSession = {
     user: {
         name: string,
@@ -51,59 +22,16 @@ type CustomSession = {
     expires: string
 }
 
-type Post = {
-    prompt: string,
-    image : string,
-    likes : number,
-    user_id: string,
-}
-
-type Friends = {
-
-}
-
 /**
  * authOptions: Configuration for authentication through next. 
  */
 const authOptions: NextAuthOptions = {  
-   // TODO - Get Jay to add a secret key to sign session/ JWT tokens
-
     /* Defines the types of ways that a user can login to the platform */
     providers: [    
-        // CredentialsProvider({
-        //     name: 'Credentials',
-        //     credentials: {
-        //         username: { label: "Username or Phone Number", type: "text", placeholder: "username or phone" },
-        //         password: { label: "Password", type: "password", placeholder: "**********" }
-        //     },
-        //     async authorize(credentials, req) {
-        //         const { username, password } = credentials as LoginCredentials
-
-        //         /* TODO - pull user from database. Reject if not known or
-        //                   redirect to register page                           */
-
-        //         /* TODO - DO THE LOGIN MAGIC */
-
-        //         return {id: '1234'}
-        //     }
-        // }),
-        // FacebookProvider({
-        //     clientId: process.env.FACEBOOK_CLIENT_ID as string,
-        //     clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string
-        // }),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
         }),
-        // InstagramProvider({
-        //     clientId: process.env.INSTAGRAM_CLIENT_ID as string,
-        //     clientSecret: process.env.INSTAGRAM_CLIENT_SECRET as string
-        // }),
-        // TwitterProvider({
-        //     clientId: process.env.TWITTER_CLIENT_ID as string,
-        //     clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
-        //     version: "2.0",
-        // })
     ],
 
     session: {
