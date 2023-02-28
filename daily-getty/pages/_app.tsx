20b4932efcdf397be9346ee5bf128f3553689ed9
jaywkym <j.w.kim@wustl.edu>
2023-02-27 19:46:59 -0600

    lightTheme

--- daily-getty/pages/_app.tsx
+++ daily-getty/pages/_app.tsx
@@ -1,12 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-export default function App({ Component, pageProps: {session, ...pageProps} }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+
+  const lightTheme = createTheme({
+    palette: {
+      mode: 'light',
+    },
+  });
 
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={lightTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   )
 }
