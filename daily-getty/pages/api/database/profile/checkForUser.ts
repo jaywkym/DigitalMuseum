84fad7732dd9a5c1f23ec135f30d169a85d666d3
wsilber <silberstein75@gmail.com>
2023-03-09 12:55:09 -0600

    Added spinner for loading images on profile

--- daily-getty/pages/api/database/profile/checkForUser.ts
+++ daily-getty/pages/api/database/profile/checkForUser.ts
@@ -113,3 +113,11 @@ function generateError(code: number, message: string, param = '', type = ''): Da
         type: type 
     }
 }
+
+export const config = {
+    api: {
+      bodyParser: {
+        sizeLimit: '10mb',
+      },
+    },
+  }
