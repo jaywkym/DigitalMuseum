5f3b84a4f1fa7223d075f6df61976257ef83552f
jaywkym <j.w.kim@wustl.edu>
2023-03-23 16:29:25 -0500

    newMuseFormUpdateModal

--- daily-getty/src/components/uploadform.tsx
+++ daily-getty/src/components/uploadform.tsx
@@ -17,17 +17,12 @@ import generatePrompt from './generateprompt';
 
 
 const MuseForm = () => {
-
     const ref1 = React.createRef();
     const ref2 = React.createRef();
     const ref3 = React.createRef();
 
-    const [value, setValue] = React.useState(''); //VALUE OF RADIO GROUP
-    const [generate, setGenerate] = React.useState(false); //SUCCESS IN GENERATING ARTWORK
-
     const [test1, settest1] = useState('')
     const [test2, settest2] = useState('')
-    const [prompt, setPrompt] = React.useState('');
 
     const timer = React.useRef<number>();
 
@@ -37,19 +32,10 @@ const MuseForm = () => {
         };
     }, []);
 
-
     //ART STYLE FORM
+    const [artStyle, setArtStyle] = React.useState(''); //VALUE OF RADIO GROUP
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setValue((event.target as HTMLInputElement).value);
-    };
-
-    //MODAL STATES
-    const handleButtonClick = () => {
-        setGenerate(true);
-    };
-
-    const handleClose = () => {
-        setGenerate(false);
+        setArtStyle((event.target as HTMLInputElement).value);
     };
 
     useMemo(() => {
@@ -57,6 +43,8 @@ const MuseForm = () => {
     }, [test1, test2])
 
     //DallE API CALL
+    const [prompt, setPrompt] = React.useState(''); //PROMPT TO GENERATE IMAGE
+
     const [b64_image1, b64_image2, b64_image3, created1, created2, created3, error, loadingImage, generateImage] = useImage(prompt, "3"); //INCORPORATE ERROR HANDLING
     const { data: session, status } = useSession()
     let user_id = status === 'authenticated' ? (session.user as any).id : "";
@@ -69,6 +57,8 @@ const MuseForm = () => {
 
 
     //IMAGE SELECTION
+    const [selected, setSelected] = React.useState(false);
+
     const imageClick = (event) => {
         let splitB64 = event.target.src.split(',')[1];
         setB64(splitB64);
@@ -80,7 +70,7 @@ const MuseForm = () => {
             id: null,
             user_id: user_id,
             userPrompt: prompt,
-            givenPrompt: null,
+            givenPrompt: question,
             likes: 0,
             image: {
                 created: createdStatic as Number,
@@ -101,38 +91,34 @@ const MuseForm = () => {
             .then(resj => {
                 console.log("good!")
             })
-
     };
 
-    const style = {
-        position: 'absolute' as 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        pt: 2,
-        px: 4,
-        pb: 3,
+    //MODAL STATES
+    const [generate, setGenerate] = React.useState(false); //SUCCESS IN GENERATING ARTWORK
+
+    const handleButtonClick = () => {
+        setGenerate(true); //Open Modal
+        setPrompt(prompt + "in the style of " + artStyle); //Update Prompt with Artstyle Value
+        generateImage(); //Generate Image
+    };
+
+    const handleClose = () => {
+        setGenerate(false); //Closes Modal
     };
 
     //GENERATE PROMPT
-
     const [question, setQuestion] = React.useState('');
-
     useEffect(() => {
 
         //QUESTION
         generatePrompt()
             .then(setQuestion)
             .catch(console.error);
-
     }, [])
 
     return (
         <Container fixed>
+            {/*Prompt Header*/}
             <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}>
                 <Box sx={{ m: 5 }}>
                     <Typography variant="h3">Prompt of the Day</Typography>
@@ -141,18 +127,21 @@ const MuseForm = () => {
                     <Typography variant="h6">{question}</Typography>
                 </Box>
             </Box>
+            {/*Answer Form (Text + Art Style Radio)*/}
             <Box sx={{ m: 5, display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', alignContent: 'space-between', alignItems: 'space-between', p: 5 }}>
+                {/*Art Style*/}
                 <FormControl>
-                    <FormLabel id="prompt" color='info'>Prompt of the Day</FormLabel>
+                    <FormLabel id="prompt" color='info'>Your Muse in Words</FormLabel>
                     <TextField id="prompt-answer" label="Answer the prompt!" variant="filled" placeholder="Enter Prompt" multiline rows={4} fullWidth required onChange={(e) => { setPrompt(e.target.value) }} />
                     <FormHelperText id="prompt-helper" color='info'>Limit your answer to 100 words or less.</FormHelperText>
                 </FormControl>
+                {/*Art Style*/}
                 <FormControl>
                     <FormLabel id="art-style">Choose an art style</FormLabel>
                     <RadioGroup
                         aria-labelledby="art-style-radio"
                         name="art-style-radio"
-                        value={value}
+                        value={artStyle}
                         onChange={handleChange}
                     >
                         <FormControlLabel value="realism" control={<Radio />} label="Realism" />
@@ -164,53 +153,8 @@ const MuseForm = () => {
                 </FormControl>
             </Box>
 
+            {/* PREVIOUS IMAGE RENDERING
             <Box sx={{ m: 5, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}>
-                {loadingImage ?
-                    <Loading>
-                        <Image id="1" alt="image" height={500} width={500} src='/placeholder.png'></Image>
-                    </Loading>
-                    :
-                    <Image id={created1} alt="image" height={500} width={500} src={b64_image1} onClick={imageClick}></Image>
-                }
-                {loadingImage ?
-                    <Loading>
-                        <Image id="2" alt="image" height={500} width={500} src='/placeholder.png'></Image>
-                    </Loading>
-                    :
-                    <Image id={created2} alt="image" height={500} width={500} src={b64_image2} onClick={imageClick}></Image>
-                }
-                {loadingImage ?
-                    <Loading>
-                        <Image id="3" alt="image" height={500} width={500} src='/placeholder.png'></Image>
-                    </Loading>
-                    :
-                    <div>
-                        <Image id={created3} alt="image" height={500} width={500} src={b64_image3} onClick={imageClick}></Image>
-                    </div>
-                }
-            </Box>
-
-            <Box sx={{ m: 5, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}>
-                <Button variant="contained" color="success" onClick={generateImage}>
-                    Generate Muse
-                </Button>
-            </Box>
-
-            {/*IMAGE GENERATED MODAL 
-                    */}
-            <Modal
-                open={generate}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                        Your Muse of the Day
-                    </Typography>
-                    <Container>
-                        {/* 
-                        <Box sx={{ m: 5, display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center', alignItems: 'center' }}>
                 {loadingImage ?
                     <Loading>
                         <Image id="1" alt="image" height={500} width={500} src='/placeholder.png'></Image>
@@ -236,22 +180,92 @@ const MuseForm = () => {
                 }
             </Box>
             */}
+
+            <Box sx={{ m: 5, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}>
+                <Button variant="contained" color="success" onClick={handleButtonClick}>
+                    Generate Muse
+                </Button>
+            </Box>
+
+            {/*IMAGE GENERATED MODAL 
+                    */}
+            <Modal
+                open={generate}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={{
+                    position: 'absolute' as 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: '80%',
+                    bgcolor: 'background.paper',
+                    border: '2px solid #000',
+                    boxShadow: 24,
+                    pt: 2,
+                    px: 4,
+                    pb: 3,
+                }}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Your Muse of the Day
+                    </Typography>
+                    <Container>
+                        <Box sx={{ m: 5, display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center', alignItems: 'center' }}>
+                            {loadingImage && !selected ?
+                                <Loading>
+                                    <Image id="1" alt="image" height={500} width={500} src='/placeholder.png'></Image>
+                                </Loading>
+                                :
+                                <Image id={created1} alt="image" height={500} width={500} src={b64_image1} onClick={imageClick}></Image>
+                            }
+                            {loadingImage && !selected ?
+                                <Loading>
+                                    <Image id="2" alt="image" height={500} width={500} src='/placeholder.png'></Image>
+                                </Loading>
+                                :
+                                <Image id={created2} alt="image" height={500} width={500} src={b64_image2} onClick={imageClick}></Image>
+                            }
+                            {loadingImage && !selected ?
+                                <Loading>
+                                    <Image id="3" alt="image" height={500} width={500} src='/placeholder.png'></Image>
+                                </Loading>
+                                :
+                                <div>
+                                    <Image id={created3} alt="image" height={500} width={500} src={b64_image3} onClick={imageClick}></Image>
+                                </div>
+                            }
+
+                            {selected &&
+                                <Box sx={{ display: 'flex', flexDirection: 'column', m: 10, p: 5 }}>
+                                    <Typography component="h1" variant="h4">
+                                        Congratulations! You have posted your muse!
+                                    </Typography>
+                                    <Button onClick={handleClose}>
+                                        Close
+                                    </Button>
+                                </Box>
+                            }
+                        </Box>
                     </Container>
                 </Box>
             </Modal>
-            {loadingImage && (
-                <CircularProgress
-                    size={68}
-                    sx={{
-                        color: green[500],
-                        position: 'absolute',
-                        top: -6,
-                        left: -6,
-                        zIndex: 1,
-                    }}
-                />
-            )}
-        </Container>
+            {
+                loadingImage && (
+                    <CircularProgress
+                        size={68}
+                        sx={{
+                            color: green[500],
+                            position: 'absolute',
+                            top: -6,
+                            left: -6,
+                            zIndex: 1,
+                        }}
+                    />
+                )
+            }
+        </Container >
     );
 };
 
