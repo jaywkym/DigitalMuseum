81ba3ca802f421961ca025d50430465c9a8c8333
William Silberstein <silberstein75@gmail.com>
2023-04-02 19:48:36 -0500

    Started on new profile page

--- daily-getty/pages/profile.tsx
+++ daily-getty/pages/profile.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import Head from 'next/head'
 import Box from '@mui/material/Box';
-import { CircularProgress } from '@mui/material';
+import { Button, CircularProgress, ImageList, ImageListItem } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import List from '@mui/material/List';
 import Post from '@/src/components/post';
 import NavBar from '@/src/components/navbar';
 import ProfileHeader from '@/src/components/profileheader';
 import { requestPostFromUserById } from '@/pages/database/posts';
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import { DatabasePost, DatabaseUser, DatabaseUserPostsResponse } from '@/types/FirebaseResponseTypes';
 import { green } from '@mui/material/colors';
 import HomeSearch from '@/src/components/homesearch';
@@ -18,9 +18,26 @@ export default function Profile() {
     const { data: session, status } = useSession();
     const [posts, setPosts] = useState([] as DatabasePost[]);
     const [loading, setLoading] = useState(false);
+    const [screenSize, setScreenSize] = useState(0)
 
     const user: DatabaseUser = session ? session.user as DatabaseUser : {} as DatabaseUser;
     const userSet = user.id !== undefined;
+    const isMobile: boolean = screenSize <= 600;
+    const isMD: boolean = screenSize <= 900
+
+    useEffect(() => {
+        setScreenSize(window.innerWidth);
+
+        window.addEventListener('resize', () => {
+            setScreenSize(window.innerWidth);
+        })
+
+        return () => {
+            window.removeEventListener("resize", () => {
+                setScreenSize(window.innerWidth);
+            })
+        }
+    }, []);
 
     async function loadImages(blankPosts) {
 
@@ -100,29 +117,68 @@ export default function Profile() {
                 <title>Profile Page</title>
             </Head>
             <main>
-            <NavBar />
-                <Box sx={{ flexGrow: 1, m: 10 }}>
-                    {/* <Button onClick={() => { signOut(); }}>Logout</Button> */}
-                    <CssBaseline />
-                    <ProfileHeader user={user} session={session}/>
-                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', alignContent: 'center' }}>
-                        {loading && (
-                            <CircularProgress
-                                size={68}
-                                sx={{
-                                    color: green[500],
+                <Box 
+                    position={'fixed'} 
+                    width={'100vw'} 
+                    height={'100vh'} 
+                    sx={{backgroundColor: 'common.blueScheme.background'}} 
+                    zIndex={-10}
+                >
+
+                </Box>
+                <NavBar isMobile={isMobile} session={session}/>
+                <Box display={'flex'} justifyContent={'end'} flexDirection={'column'} alignItems={'end'}>
+                    <Box 
+                        sx={{
+                            width: {xs: '100%', sm: '90%', md: '80%'}, 
+                        }}
+
+                        display={'flex'}
+                        justifyContent={'center'}
+                        padding={4}
+                    >
+
+                        <Box sx={{ flexGrow: 1}}>
+                            <CssBaseline />
+                            <ProfileHeader user={user} session={session}/>
+                            <Box 
+                                sx={{ 
+                                    display: 'flex', 
+                                    justifyContent: 'center', 
+                                    alignItems: 'center', 
+                                    alignContent: 'center' 
                                 }}
-                            />
-                        )}
-                        <List>
-                            {
-                                Object.keys(posts_map).map((post) => (
-                                    <Post _userObj={user} _post={posts[post]} key={posts[post].id} session={session} />
-                                ))
-                            }
-                        </List>
+
+                                marginTop={4}
+                            >
+                                {loading && (
+                                    <CircularProgress
+                                        size={68}
+                                        sx={{
+                                            color: green[500],
+                                        }}
+                                    />
+                                )}
+
+                                <ImageList cols={isMobile? 1 : isMD? 2 : 3} gap={20}>
+
+                                {
+                                            
+                                    posts.map((post, i) => (
+                                        <ImageListItem key={i} >
+                                            <Post _userObj={user} _post={post} key={post.user_id + "-" + post.id} session={session}/>
+                                        </ImageListItem>
+                                    ))
+                                }
+
+                                </ImageList>
+                            </Box>
+                        </Box>
+                        
+
                     </Box>
                 </Box>
+                
                 
             </main>
         </>
